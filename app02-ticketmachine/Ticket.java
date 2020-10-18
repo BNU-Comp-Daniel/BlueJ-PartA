dda6ecb65f14816e4016f8e44af20c0deeaaf51b
DanielHale99 <72168379+DanielHale99@users.noreply.github.com>
2020-10-18 19:02:47 +0100

    Update Ticket.java

--- app02-ticketmachine/Ticket.java
+++ app02-ticketmachine/Ticket.java
@@ -1,33 +1,32 @@
-
+import java.util.Date;
 /**
- * Write a description of class Ticket here.
+ * This class represents the ticket that is being purchased, it shall contain
+ * the date, time, price and destination for that ticket.
  *
  * @author (Daniel Hale)
  * @version (18/10/2020)
  */
 public class Ticket
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // Attributes
+    
+    private String destination;
+    
+    private int price;
+    
+    private Date issueDateTime;
 
     /**
-     * Constructor for objects of class Ticket
+     * This sets the destination and price of the ticket, the date and time
+     * shall also match that of the current date and time on the system 
+     * being used.
      */
-    public Ticket()
+    public Ticket(String destination, int price)
     {
-        // initialise instance variables
-        x = 0;
+        this.destination = destination;
+        this.price = price;
+        
+        issueDateTime = new Date();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
